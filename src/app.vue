e92372c9b05f88826059d8e00a53c9bafeeba65d
RanranLiu111 <864749024@qq.com>
2019-10-09 16:14:27 +0800

    webpack的配置 webpackloader的添加

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">{{text}}</div>  
+</template>
+
+<script>
+export default {
+  data(){
+    return {text:'abcwsddssdsdd'}
+  }
+}
+</script>
+
+<style>
+ #app{
+   color: red;
+ }
+</style>
