c07875e8aeb6e2c7c022e9f0371001a7c70b5c52
RanranLiu111 <864749024@qq.com>
2019-10-12 17:05:20 +0800

    修改webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const isDev = process.env.NODE_ENV === 'development';
 
 let config = {
   target:'web',//编译目标为web平台
-  entry: path.join(__dirname,'src/index.js'),
+  entry: path.join(__dirname,'client/index.js'),
   output: {
     filename:'boundle.js',
     path:path.join(__dirname,'dist')
