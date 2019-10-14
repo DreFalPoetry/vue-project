96dbe12de6129149cce9c666bedc18e04f85fa73
RanranLiu111 <864749024@qq.com>
2019-10-14 16:07:28 +0800

    babel转译es6

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// import '@babel/polyfill';
+
+//测试es6语法是都通过babel转译
+const array = [1,2,3]
+const isES6 = () => console.log(...array)
+isES6()
+
+const arr = [new Promise(()=>{}),new Promise(()=>{})]
+arr.map(item => {console.log(item)})
+
 //es6
 import sum from './vendor/sum';
 console.log('sum(1,2) = ',sum(1,2))
