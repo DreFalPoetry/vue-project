d4b4ea9e307fed0d66c5055575345e596b70f65f
RanranLiu111 <864749024@qq.com>
2019-10-15 15:39:43 +0800

    设置css module 使样式局部有效

--- primary/src/index.js
+++ primary/src/index.js
@@ -1,9 +1,12 @@
+import style from  './index.scss';
 import avatar from './avatar.png'
-import './index.scss';
+import createAvatar from './createAvatar'
+
+createAvatar()
 
 var img = new Image()
 img.src = avatar;
-img.classList.add('avatar');
+img.classList.add(style.avatar);
 
 var root = document.getElementById('root')
 root.append(img);
