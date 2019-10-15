d91bdf39027bab8d0f6cc901b84909b14e9e6fb7
RanranLiu111 <864749024@qq.com>
2019-10-15 18:53:41 +0800

    自动生成html以及打包之前删除打包文件

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ webpack is a module boundler  【模块打包工具】
 
 loader ==> 识别非js结尾的模块  执行顺序【从上到下，从右到左】
 
+htmlWebpackPlugin 插件 : 在打包结束后自动生成一个html文件，并把打包生成的js自动引入到这个html中
+
+CleanWebpackPlugin 插件，每次打包之前删除打包文件
+
+##### plugin   在webpack运行到某个时刻的时候帮你做一些事情
+
--- primary/webpack.config.js
+++ primary/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -38,5 +40,11 @@ module.exports = {
   output: {
     filename:'bundle.js',
     path: path.resolve(__dirname,'dist'),
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template:'src/index.html'
+    })
+  ]
 }
