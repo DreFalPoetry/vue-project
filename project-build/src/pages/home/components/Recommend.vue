39aa0bf6faa505b4969fa527de47c16d824f175a
Racky <racky.liu@moca-tech.net>
2019-11-28 15:16:02 +0800

    recommend 部分 和 icon部分样式

---
+++ project-build/src/pages/home/components/Recommend.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommandList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommandList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+   @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 .1rem
+        border-radius .06rem
+        color #fff
+</style>
