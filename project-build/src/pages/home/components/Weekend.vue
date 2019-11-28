f15eb3600521ca3dbef4948e3109cb1e07748076
Racky <racky.liu@moca-tech.net>
2019-11-28 15:36:59 +0800

    add weekend

---
+++ project-build/src/pages/home/components/Weekend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommandList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommandList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1811/e4/e4b9b471d2afc473a3.img.jpg_200x200_9a7794fc.jpg',
+        title: '东方明珠(AAAAA景区)',
+        desc: '东方明珠看上海·超值票成人票'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+   @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+</style>
